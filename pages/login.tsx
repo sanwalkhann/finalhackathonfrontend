1c0c3c83f4db05ae18d3bd505db601e5453c7145
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2024-02-17 15:21:54 +0500

    ...final

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,7 +9,7 @@ const LoginPage: React.FC = () => {
   const [password, setPassword] = useState("");
   const handleLogin = async () => {
     try {
-      const response = await fetch("http://localhost:3000/auth/login", {
+      const response = await fetch("https://finalhacathonbackend.vercel.app/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
